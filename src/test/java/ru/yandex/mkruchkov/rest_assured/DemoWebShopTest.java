5c10c92a41c027cccee87715012083c408aee9e0
Максим <Raboznik@yandex.ru>
2022-03-08 00:22:34 +0300

    add 10-18 HW

--- src/test/java/ru/yandex/mkruchkov/rest_assured/DemoWebShopTest.java
+++ src/test/java/ru/yandex/mkruchkov/rest_assured/DemoWebShopTest.java
@@ -1,18 +1,22 @@
 package ru.yandex.mkruchkov.rest_assured;
 
-import org.hamcrest.Matchers;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 
 public class DemoWebShopTest {
 
+    Faker faker = new Faker();
+
     @DisplayName("Negative test for newsletter")
     @Test
     void newsletterNegativeTest() {
+
         given()
                 .contentType("text/html; charset=utf-8")
                 .body("email=test")
@@ -28,9 +32,10 @@ public class DemoWebShopTest {
     @DisplayName("Test for newsletter")
     @Test
     void newsletterTest() {
+
         given().
                 contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .body("email=test@test.ru")
+                .body("email=" + faker.internet().emailAddress())
                 .when()
                 .post("http://demowebshop.tricentis.com/subscribenewsletter")
                 .then()
@@ -38,6 +43,22 @@ public class DemoWebShopTest {
                 .statusCode(200)
                 .body("Success", is(true))
                 .body("Result", is("Thank you for signing up! A verification email has been sent. We appreciate your interest."));
+    }
 
+    @DisplayName("Test for adding product to wishlist")
+    @Test
+    void addProductToWishlistTest() {
+
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("NOPCOMMERCE.AUTH=C36C70AB2001AB8E7287F00E46DEE2C2FB8BCC76A4030C044DF8791B17602AB2E174EA56613DC03037366B2E197312C0A3919987D291BEECCDBFDED44B02F0327DBB5D1803759EDD090CD380990E832730EA5A8A1E9835CC186CFF5CF06983DFCB0DF0E007E449DDAD8695EA76D8F8962E6516CCBF352C89444725A498338B4C;")
+                .when()
+                .post("http://demowebshop.tricentis.com/addproducttocart/details/65/2")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("success", is(true))
+                .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"))
+                .body("updatetopwishlistsectionhtml", is(notNullValue()));
     }
 }
